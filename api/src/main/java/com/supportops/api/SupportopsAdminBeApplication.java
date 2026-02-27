953a42d2a2e968d79342da866c67662179db895f
Than Nguyen Nham <paul.nguyen@glohow.com>
2026-02-27 11:09:27 +0700

    chore: bootstrap Spring Boot project

---
+++ api/src/main/java/com/supportops/api/SupportopsAdminBeApplication.java
@@ -0,0 +1,13 @@
+package com.supportops.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportopsAdminBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportopsAdminBeApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/supportops/api/SupportopsAdminBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supportops.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportopsAdminBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
